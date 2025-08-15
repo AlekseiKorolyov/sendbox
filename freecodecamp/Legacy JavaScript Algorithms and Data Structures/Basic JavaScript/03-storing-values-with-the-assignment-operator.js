3e1468569c77027108abf56d7ff4b01bd8b78cb5
Aleksei Korolyov <a97415393@gmail.com>
2025-08-15 17:27:13 +0300

    Storing Values with the Assignment Operator

---
+++ freecodecamp/Legacy JavaScript Algorithms and Data Structures/Basic JavaScript/03-storing-values-with-the-assignment-operator.js
@@ -0,0 +1,11 @@
+// Instructions:
+// 1. You should not change code above the specified comment.
+// 2. a should have a value of 7.
+
+// Solution:
+// Setup
+var a;
+
+// Only change code below this line
+
+a = 7;
