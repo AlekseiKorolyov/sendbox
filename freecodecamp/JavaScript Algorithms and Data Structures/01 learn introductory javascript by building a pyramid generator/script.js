7a5a288db51d8831884326ae1073e1d1f8f6d582
Aleksei Korolyov <a97415393@gmail.com>
2025-09-23 09:44:44 +0300

    js 1 Step 26

--- freecodecamp/JavaScript Algorithms and Data Structures/01 learn introductory javascript by building a pyramid generator/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/01 learn introductory javascript by building a pyramid generator/script.js
@@ -1,6 +1,7 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");                              /* метод .push(). Он позволяет «поместить» значение в конец массива */
 console.log(rows);
 
 
@@ -14,4 +15,4 @@ console.log(rows);
 
 
 // console.log(rows[0]);
-// rows[rows.length - 1] = 10;         /* изменяет последний элемент массива (возвращает количество элементов в массиве вычитая 1) */
+// rows[rows.length - 1] = 10;         /* .langth изменяет последний элемент массива (возвращает количество элементов в массиве вычитая 1) */
