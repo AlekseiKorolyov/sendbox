eceee6a7bdf6e39080d0aece656489928fd350b5
Aleksei Korolyov <a97415393@gmail.com>
2025-09-23 09:21:14 +0300

    js 1 Step 23

--- freecodecamp/JavaScript Algorithms and Data Structures/01 learn introductory javascript by building a pyramid generator/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/01 learn introductory javascript by building a pyramid generator/script.js
@@ -1,6 +1,17 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows[0]);
-rows[rows.length - 1] = 10;         /* возвращает количество элементов в массиве вычитая 1 */
 console.log(rows);
+
+
+
+
+
+
+
+
+
+
+
+// console.log(rows[0]);
+// rows[rows.length - 1] = 10;         /* возвращает количество элементов в массиве вычитая 1 */
