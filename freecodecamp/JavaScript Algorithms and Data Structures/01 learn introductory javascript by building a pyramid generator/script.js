0982639786c9c6ab53d21abc164e0e1887acbe62
Aleksei Korolyov <a97415393@gmail.com>
2025-09-28 09:29:17 +0300

    js 1 Step 95

--- freecodecamp/JavaScript Algorithms and Data Structures/01 learn introductory javascript by building a pyramid generator/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/01 learn introductory javascript by building a pyramid generator/script.js
@@ -11,7 +11,7 @@ function padRow(rowNumber, rowCount) {
 
 let done = 0;
 
-while (done <= count) {
+while (rows.length < count) {
     done++;
     rows.push(padRow(done, count));
 }
@@ -30,10 +30,80 @@ console.log(result);
 
 
 
+// while (continueLoop) {
+//     done++;
+//     rows.push(padRow(done, count));
+//     if (done === count) {
+//         continueLoop = false;
+//     }
+// }
+
+// done++;  используется без пробела
+
+// if ("") {
+//     console.log("Condition is true");
+// } else if (5 > 10) {
+//     console.log("5 is less than 10");
+// } else {
+//     console.log("This is the else block");
+// }
 
 
-/*
+
+//Создайте оператор if с логическим значением true в качестве условия. В теле оператора выведите строку «Условие истинно».
+// if (true) {
+//     console.log("Condition is true");
+// }
 
 
 
- */
+/* */
+
+// function padRow(rowNumber, rowCount) {                        функция padRow возвращает значение символьной переменной, повторяющееся rowNumber раз
+//     return " " + character.repeat(rowNumber)  + " ";          " " + character + " " добавляет по одному пробелу « » в начало и конец повторяющейся строки символов.
+//     return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);   повторяет rowCount - rowNumber раз
+// }
+
+
+
+// function padRow(name) {                              /* создание функции padRow */
+//     const test = "Testing";           Переменные объявленные внутри функции считаются находящимися в локальной области видимости, или области видимости блока. Переменная, объявленная внутри функции, может использоваться только внутри этой функции.
+//     return name;                                     /*  По умолчанию функции возвращают undefined в качестве своего значения. Чтобы вернуть что-то другое, необходимо использовать ключевое слово return */
+// }                                                    /* код после return не работает */
+// const call = padRow("AIK");                           /* вызов функцю padRow */
+// console.log(call);
+
+// for (let i = 0; i < count; i = i + 1) {
+//     rows.push(i);                                   /* помещает i в массив строк. */
+//     rows.push(character.repeat(i + 1));          /* помещает переменную в массив строк */
+// }                                                      /* .repeat(i) Этот метод принимает число в качестве аргумента, указывающее количество повторений целевой строки переменная i представляет собой текущий номер «строки» в вашем цикле */
+// /* повторение строки ноль раз не приводит к выводу каких-либо данных. Чтобы исправить это, добавьте 1 к значению i в вызове .repeat() */
+// let result = "";
+//
+// for (const row of rows) {                          /* цикл for...of перебирает каждый элемент итерируемого объекта и временно присваивает его переменной */
+//     result = row + result + "\n";                  /* hello = hello + "World"; добавит строку "World" к существующей строке, хранящейся в переменной hello. Это называется конкатенацией. */
+// }                                                  /*  + "\n"   переносит элемент массива на новую строку   интерпретируется как новая строка при записи строки в журнал */
+//
+// console.log(result);
+
+// function addTwoNumbers(firstName, lastName) {   /* создаю функцию с двумя аргументами */
+//     return firstName + lastName;                /* возвращается сумма двух аргументов */
+// }
+//
+// const sum = addTwoNumbers(5, 10);               /* объявляется переменная sum и вызывает функцию */
+// console.log(sum);                               /* выводит в кансоль значение sum */
+
+//for (iterator; condition; iteration) {
+//   logic;
+// }
+// Пример кода for (итератор; условие; итерация) { логика; }
+
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// rows.push("freeCodeCamp");          /* метод .push() позволяет «поместить» значение в конец массива */
+// let pushed = rows.push();
+// console.log(pushed);
+// let popped = rows.pop();            /* метод .pop() удаляет последний элемент из массива и возвращает этот элемент */
+// console.log(popped);
+// console.log(rows);
+// console.log(rows[0]);
+// rows[rows.length - 1] = 10;         /* .length изменяет последний элемент массива (возвращает количество элементов в массиве вычитая 1) */
