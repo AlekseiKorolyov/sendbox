82025bb012fe085afc85f35d302c68369f8d8f2c
Aleksei Korolyov <a97415393@gmail.com>
2025-09-29 08:38:08 +0300

    js 1 Step 113

--- freecodecamp/JavaScript Algorithms and Data Structures/01 learn introductory javascript by building a pyramid generator/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/01 learn introductory javascript by building a pyramid generator/script.js
@@ -7,6 +7,9 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
  for (let i = 1; i <= count; i ++) {
+     if (inverted){
+
+     }
      rows.unshift(padRow(i, count));
  }
 
