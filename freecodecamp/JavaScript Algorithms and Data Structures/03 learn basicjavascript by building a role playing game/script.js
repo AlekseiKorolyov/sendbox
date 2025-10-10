96dc58b1cae61815a265d182839d796aca270690
Aleksei Korolyov <a97415393@gmail.com>
2025-10-10 12:45:16 +0300

    js 3 Step 132

--- freecodecamp/JavaScript Algorithms and Data Structures/03 learn basicjavascript by building a role playing game/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/03 learn basicjavascript by building a role playing game/script.js
@@ -180,6 +180,8 @@ function dodge () {
 function defeatMonster () {
     gold += Math.floor(monsters[fighting].level * 6.7) + gold;
     xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
 }
 
 function lose () {
