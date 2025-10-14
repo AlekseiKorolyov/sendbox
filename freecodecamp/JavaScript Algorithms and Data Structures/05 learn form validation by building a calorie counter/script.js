9735b4d724607d07406e336fdf6a599540caa991
Aleksei Korolyov <a97415393@gmail.com>
2025-10-14 12:09:48 +0300

    js 5 Step 17

--- freecodecamp/JavaScript Algorithms and Data Structures/05 learn form validation by building a calorie counter/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/05 learn form validation by building a calorie counter/script.js
@@ -4,3 +4,51 @@ const entryDropdown = document.getElementById("entry-dropdown");
 const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
+let isError = false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+17          is или has
+
+
+
+
+Шаг 17
+
+В программировании добавление префикса is или has к переменной — распространённая практика, указывающая на то, что переменная представляет собой логическое значение. Вот несколько примеров:
+Пример кода
+
+let isRunning = true;
+let hasCompleted = false;
+
+Объявите переменную с именем isError с помощью let и инициализируйте её значением false, что позволит впоследствии переназначить её. Позже в проекте вы обновите значение isError, если пользователь введёт недопустимое значение.
+
+
+
+
+
+ */
