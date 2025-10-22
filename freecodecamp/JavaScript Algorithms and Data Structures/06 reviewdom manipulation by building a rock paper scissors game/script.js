b22f78e1628487a5c738b07853dbe305777fa292
Aleksei Korolyov <a97415393@gmail.com>
2025-10-22 14:27:40 +0300

    js 6 Step 1

---
+++ freecodecamp/JavaScript Algorithms and Data Structures/06 reviewdom manipulation by building a rock paper scissors game/script.js
@@ -0,0 +1,51 @@
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+
+}
+console.log(getRandomComputerResult());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Шаг 1
+
+Первым шагом является создание функции, которая будет генерировать случайный выбор для компьютера. Функция getRandomComputerResult будет использоваться для получения выбора компьютера. Внутри этой функции вы должны увидеть массив вариантов с «Камнем», «Бумагой» и «Ножницами». Ваша задача — дополнить функцию getRandomComputerResult так, чтобы она возвращала случайный вариант из массива вариантов. Советы Вы можете использовать функции Math.random() и Math.floor() для получения случайного целого числа. Это будет индекс массива вариантов. Вы можете использовать случайный индекс для доступа к варианту из массива вариантов.
+
+
+function getRandomComputerResult() {
+  const options = ["Rock", "Paper", "Scissors"];
+
+}
+
+***
+
+function getRandomComputerResult() {
+  const options = ["Rock", "Paper", "Scissors"];
+return options[Math.floor(Math.random() * options.length)];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ */
