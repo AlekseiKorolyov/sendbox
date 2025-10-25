dfad6e4acc40e20298ac5e4bcb0ca090832fd577
Aleksei Korolyov <a97415393@gmail.com>
2025-10-25 14:35:29 +0300

    js 7 Step 5

--- freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
@@ -12,6 +12,13 @@ let allSongs = [
         artist: "Quincy Larson",
         duration: "4:25",
         src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+    },
+    {
+        id: 1,
+        title: "Can't Stay Down",
+        artist: "Quincy Larson",
+        duration: "4:15",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
     }
 ];
 
@@ -65,6 +72,28 @@ src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-sur
   artist: "Quincy Larson",
   duration: "4:25",
   src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+}
+
+
+
+Шаг 5
+
+Добавьте второй объект со следующими ключами и значениями:
+Пример кода
+
+id: 1,
+title: "Can't Stay Down",
+artist: "Quincy Larson",
+duration: "4:15",
+src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
+
+
+{
+  id: 1,
+  title: "Can't Stay Down",
+  artist: "Quincy Larson",
+  duration: "4:15",
+  src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
 }
 
 
