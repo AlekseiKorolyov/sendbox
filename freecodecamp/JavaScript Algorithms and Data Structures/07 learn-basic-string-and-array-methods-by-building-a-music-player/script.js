ac29e383604bb3cb12c3dc27550197f0f430c4f1
Aleksei Korolyov <a97415393@gmail.com>
2025-10-25 14:25:09 +0300

    js 7 Step 3

--- freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
@@ -1,11 +1,11 @@
 const playlistSongs = document.getElementById("playlist-songs");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
-
 const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
+let allSongs = [];
 
 
 
@@ -28,6 +28,14 @@ const pauseButton = document.getElementById("pause");
 const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
+
+
+
+Шаг 3
+
+Далее создайте массив для хранения всех песен. Создайте пустой массив allSongs.
+
+let allSongs = [];
 
 
 
