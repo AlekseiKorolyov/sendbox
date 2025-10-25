87ab38c16c8be67fec5a4c7b38ee4b41c371832b
Aleksei Korolyov <a97415393@gmail.com>
2025-10-25 13:32:03 +0300

    js 7 Step 1

---
+++ freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
@@ -0,0 +1,21 @@
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+
+
+
+
+
+
+/*
+Шаг 1 В этом проекте вы изучите основные методы работы со строками и массивами, создав приложение музыкального проигрывателя. Вы сможете воспроизводить, приостанавливать, пропускать и перемешивать песни. HTML и CSS этого проекта уже предоставлены, поэтому вы можете сосредоточиться на JavaScript. Начнем с доступа к элементам #playlist-songs, #play и #pause с помощью метода getElementById(). Присвойте их переменным playlistSongs, playButton и pauseButton соответственно.
+
+***
+
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+
+
+
+ */
