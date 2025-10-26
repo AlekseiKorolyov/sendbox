3054a2789153b47d22b0c39b3f7b03068633bdf9
Aleksei Korolyov <a97415393@gmail.com>
2025-10-26 10:11:37 +0300

    js 7 Step 7

--- freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
@@ -26,8 +26,82 @@ let allSongs = [
         artist: "Quincy Larson",
         duration: "3:51",
         src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
-    }
+    },
+    {
+        id: 3,
+        title: "Cruising for a Musing",
+        artist: "Quincy Larson",
+        duration: "3:34",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cruising-for-a-musing.mp3",
+    },
+    {
+        id: 4,
+        title: "Never Not Favored",
+        artist: "Quincy Larson",
+        duration: "3:35",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/never-not-favored.mp3",
+    },
+    {
+        id: 5,
+        title: "From the Ground Up",
+        artist: "Quincy Larson",
+        duration: "3:12",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/from-the-ground-up.mp3",
+    },
+    {
+        id: 6,
+        title: "Walking on Air",
+        artist: "Quincy Larson",
+        duration: "3:25",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/walking-on-air.mp3",
+    },
+    {
+        id: 7,
+        title: "Can't Stop Me. Can't Even Slow Me Down.",
+        artist: "Quincy Larson",
+        duration: "3:52",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cant-stop-me-cant-even-slow-me-down.mp3",
+    },
+    {
+        id: 8,
+        title: "The Surest Way Out is Through",
+        artist: "Quincy Larson",
+        duration: "3:10",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/the-surest-way-out-is-through.mp3",
+    },
+    {
+        id: 9,
+        title: "Chasing That Feeling",
+        artist: "Quincy Larson",
+        duration: "2:43",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/chasing-that-feeling.mp3",
+    },
 ];
+
+const audio = new Audio();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -103,5 +177,12 @@ src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp
   src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
 }
 
+
+
+Шаг 7
+
+Мы добавили оставшиеся песни в массив allSongs. Далее вы узнаете об API веб-аудио и о том, как использовать его для воспроизведения песен. Все современные браузеры поддерживают API веб-аудио, который позволяет генерировать и обрабатывать аудио в веб-приложениях. Используйте const для создания переменной с именем audio и присвойте ей значение new Audio(). Это создаст новый элемент HTML5 audio.
+
+const audio = new Audio();
 
  */
