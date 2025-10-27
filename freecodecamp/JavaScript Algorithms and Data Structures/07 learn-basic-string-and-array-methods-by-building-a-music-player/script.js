a731ca3ce09b21342675f124514220d9485ac530
Aleksei Korolyov <a97415393@gmail.com>
2025-10-27 10:38:32 +0300

    js 7 Step 24

--- freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
@@ -101,7 +101,7 @@ const renderSongs = array => {
                 </svg>
             </button>
         `;
-    });
+    }).join("");
 }
 
 
@@ -415,7 +415,34 @@ return `<li id="song-${song.id}" class="playlist-song"></li>`;
     </button>
 
 
+Шаг 24
 
+Сейчас songsHTML — это массив. Если вы попытаетесь отобразить его как есть, вы увидите песни, разделенные запятыми. Это нежелательно, поскольку мы хотим отобразить песни в виде списка. Чтобы исправить это, вам нужно объединить массив в одну строку с помощью метода join(). Метод join() используется для объединения всех элементов массива в одну строку. Он принимает необязательный параметр, называемый разделителем, который используется для разделения каждого элемента массива. Например:
+Пример кода
+
+const exampleArr = ["This", "is", "a", "sentence"];
+const sentence = exampleArr.join(" ");// Разделитель принимает символ пробела
+console.log(sentence); // Вывод: "This is a sentence"
+
+Свяжите метод join() с методом map() и передайте пустую строку в качестве разделителя. Чтобы объединить несколько методов в цепочку, вы можете вызвать метод join() для результата метода map(). Например: Пример кода array.map().join();
+
+const renderSongs = (array) => {
+  const songsHTML = array
+    .map((song)=> {
+      return `
+      <li id="song-${song.id}" class="playlist-song">
+      <button class="playlist-song-info">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+      </button>
+      <button class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
+      </button>
+      </li>
+      `;
+    }).join("");
 
 
 
