ba6dc07f98d485e258960da1dba07380ff5ee2a0
Aleksei Korolyov <a97415393@gmail.com>
2025-11-06 11:16:00 +0300

    js 7 Step 72

--- freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
@@ -5,7 +5,7 @@ const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
-let allSongs = [
+const allSongs = [
     {
         id: 0,
         title: "Scratching The Surface",
@@ -79,7 +79,6 @@ let allSongs = [
 ];
 
 const audio = new Audio();
-
 let userData = {
     songs: [...allSongs],
     currentSong: null,
@@ -90,13 +89,15 @@ const playSong = (id) => {
     const song = userData?.songs.find((song) => song.id === id);
     audio.src = song.src;
     audio.title = song.title;
+
     if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
         audio.currentTime = 0;
-    }  else {
+    } else {
         audio.currentTime = userData?.songCurrentTime;
     }
     userData.currentSong = song;
     playButton.classList.add("playing");
+
     highlightCurrentSong();
     setPlayerDisplay();
     audio.play();
@@ -104,6 +105,7 @@ const playSong = (id) => {
 
 const pauseSong = () => {
     userData.songCurrentTime = audio.currentTime;
+
     playButton.classList.remove("playing");
     audio.pause();
 };
@@ -114,16 +116,17 @@ const playNextSong = () => {
     } else {
         const currentSongIndex = getCurrentSongIndex();
         const nextSong = userData?.songs[currentSongIndex + 1];
+
         playSong(nextSong.id);
     }
 };
 
 const playPreviousSong = () => {
-    if (userData?.currentSong === null) {
-        return;
-    } else {
+    if (userData?.currentSong === null) return;
+    else {
         const currentSongIndex = getCurrentSongIndex();
         const previousSong = userData?.songs[currentSongIndex - 1];
+
         playSong(previousSong.id);
     }
 };
@@ -133,6 +136,7 @@ const setPlayerDisplay = () => {
     const songArtist = document.getElementById("player-song-artist");
     const currentTitle = userData?.currentSong?.title;
     const currentArtist = userData?.currentSong?.artist;
+
     playingSong.textContent = currentTitle ? currentTitle : "";
     songArtist.textContent = currentArtist ? currentArtist : "";
 };
@@ -147,34 +151,34 @@ const highlightCurrentSong = () => {
         songEl.removeAttribute("aria-current");
     });
 
-    if (songToHighlight) {
-        songToHighlight.setAttribute("aria-current", "true");
-    }
+    if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
-const renderSongs = array => {
-    const songsHTML = array.map((song) => {
-        return `<li id="song-${song.id}" class="playlist-song"></li>
-            <button class="playlist-song-info" onclick="playSong(${song.id})">
-                <span class="playlist-song-title">${song.title}</span>
-                 <span class="playlist-song-artist">${song.artist}</span>
-                 <span class="playlist-song-duration">${song.duration}</span>
-            </button>
-            <button class="playlist-song-delete" aria-label="Delete ${song.title}">
-                <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="8" cy="8" r="8" fill="#4d4d62"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/>
-                </svg>
-            </button>
-        `;
-    }).join("");
+const renderSongs = (array) => {
+    const songsHTML = array
+        .map((song)=> {
+            return `
+      <li id="song-${song.id}" class="playlist-song">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+      </button>
+      <button class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
+        </button>
+      </li>
+      `;
+        })
+        .join("");
 
     playlistSongs.innerHTML = songsHTML;
 };
 
-const getCurrentSongIndex = () => {
-    return userData?.songs.indexOf(userData?.currentSong);
-};
+const setPlayButtonAccessibleText = () => {};
+
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
 playButton.addEventListener("click", () => {
     if (userData?.currentSong === null) {
@@ -184,8 +188,10 @@ playButton.addEventListener("click", () => {
     }
 });
 
-pauseButton.addEventListener("click", pauseSong);
+pauseButton.addEventListener("click",  pauseSong);
+
 nextButton.addEventListener("click", playNextSong);
+
 previousButton.addEventListener("click", playPreviousSong);
 
 const sortSongs = () => {
@@ -193,11 +199,14 @@ const sortSongs = () => {
         if (a.title < b.title) {
             return -1;
         }
+
         if (a.title > b.title) {
             return 1;
         }
+
         return 0;
     });
+
     return userData?.songs;
 };
 
@@ -1110,6 +1119,14 @@ const example = condition ? "I'm true" : "I'm false";
 
 setPlayerDisplay();
 
+
+
+Шаг 72
+
+Чтобы сделать приложение более доступным, важно, чтобы кнопка воспроизведения описывала текущую песню или первую песню в плейлисте. Начнем с создания пустой стрелочной функции с именем setPlayButtonAccessibleText.
+
+
+const setPlayButtonAccessibleText = () => {};
 
 
 
