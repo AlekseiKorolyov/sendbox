2178d3a8cf31e2da0a19ae006589613ba21b1785
Aleksei Korolyov <a97415393@gmail.com>
2025-11-06 10:11:01 +0300

    js 7 Step 68

--- freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/07 learn-basic-string-and-array-methods-by-building-a-music-player/script.js
@@ -127,7 +127,10 @@ const playPreviousSong = () => {
     }
 };
 
-const setPlayerDisplay = () => {};
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+};
 
 const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll(".playlist-song");
@@ -1042,6 +1045,22 @@ highlightCurrentSong();
 
 
 const setPlayerDisplay = () => {};
+
+
+
+
+
+Шаг 68
+
+Внутри функции получите ссылки на HTML-элементы, отвечающие за отображение названия песни и исполнителя. Доступ к элементам #player-song-title и #player-song-artist осуществляется с помощью метода getElementById(). Присвойте их переменным playingSong и songArtist соответственно.
+
+
+const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+};
+
+
+
 
 
  */
