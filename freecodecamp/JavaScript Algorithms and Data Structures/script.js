1f366fb6a2629170dd9bbe426c0f71c408401cd1
Aleksei Korolyov <a97415393@gmail.com>
2025-11-24 12:55:51 +0300

    js 9 Step 4

--- freecodecamp/JavaScript Algorithms and Data Structures/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/script.js
@@ -2,6 +2,7 @@ const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
 const date = new Date();
+const day = date.getDate();
 
 
 
@@ -43,6 +44,22 @@ const date = new Date();
 
 
 const date = new Date();
+
+
+
+Шаг 4
+
+Объект Date имеет ряд методов, позволяющих получать дату и время в различных форматах. Один из них — метод .getDate(), который возвращает число от 1 до 31, представляющее день месяца для этой даты. Например:
+Пример кода
+
+const date = new Date();
+const dayOfTheMonth = date.getDate();
+console.log(dayOfTheMonth); // 20
+
+Используя const, создайте переменную с именем day и присвойте ей день месяца из date с помощью метода .getDate().
+
+
+const day = date.getDate();
 
 
 
