2a95545b66ed6016debf76b3ba6037b38a41192f
Aleksei Korolyov <a97415393@gmail.com>
2025-11-24 12:55:19 +0300

    js 9 Step 3

--- freecodecamp/JavaScript Algorithms and Data Structures/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/script.js
@@ -1,2 +1,53 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
+
+const date = new Date();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+Шаг 3
+
+ В JavaScript существует множество встроенных конструкторов, создающих объекты. Конструктор похож на обычную функцию, но начинается с заглавной буквы и инициализируется оператором new. Например, вы можете использовать конструктор Date() с оператором new для создания нового объекта Date, который возвращает строку с текущей датой и временем:
+ Пример кода
+
+ const currentDate = new Date();
+ console.log(currentDate);
+ // Вывод:
+ // Пн, 23 августа 2021 г., 15:31:00 GMT-0400 (восточное летнее время)
+
+ Создайте новую константную переменную с именем date и присвойте ей объект Date с помощью оператора new Date().
+
+
+const date = new Date();
+
+
+
+
+
+
+
+ */
