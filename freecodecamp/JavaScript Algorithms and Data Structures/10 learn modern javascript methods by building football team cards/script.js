2f7dd5d78cff14c30c3de6f3fb86b1c2d09f19fd
Aleksei Korolyov <a97415393@gmail.com>
2025-11-26 11:23:18 +0300

    js 10 Step 5

--- freecodecamp/JavaScript Algorithms and Data Structures/10 learn modern javascript methods by building football team cards/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/10 learn modern javascript methods by building football team cards/script.js
@@ -4,3 +4,46 @@ const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
+const myFavoriteFootballTeam = {};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Шаг 5
+
+Теперь пора создать структуру данных, которая будет хранить всю информацию о вашей футбольной команде. Под переменными, которые вы только что создали, создайте новую константную переменную с именем myFavoriteFootballTeam и присвойте ей пустой объект.
+
+
+const myFavoriteFootballTeam = {};
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
