4b8398e5a0219a629b267ff733c4c38cedafe238
Aleksei Korolyov <a97415393@gmail.com>
2025-11-28 13:43:19 +0300

    js 11 Step 5

--- freecodecamp/JavaScript Algorithms and Data Structures/11 learn localstorage by building a todo app/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/11 learn localstorage by building a todo app/script.js
@@ -9,3 +9,44 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+
+const taskData = [];
+let currentTask = {};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+Шаг 5
+
+Создайте константу taskData и присвойте ей пустой массив. Этот массив будет хранить все задачи вместе с их данными, включая название, срок выполнения и описание. Это хранилище позволит вам отслеживать задачи, отображать их на странице и сохранять в localStorage. Используйте let для создания переменной currentTask и присвойте ей пустой объект. Эта переменная будет использоваться для отслеживания состояния при редактировании и отмене задач.
+
+
+const taskData = [];
+let currentTask = {};
+
+
+
+
+ */
