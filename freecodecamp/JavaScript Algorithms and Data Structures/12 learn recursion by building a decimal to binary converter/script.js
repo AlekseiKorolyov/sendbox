29c25ca8d819a01a3bb2a4ebc383ad86a024cdb6
Aleksei Korolyov <a97415393@gmail.com>
2025-12-04 06:59:27 +0300

    js 12 Step 1

---
+++ freecodecamp/JavaScript Algorithms and Data Structures/12 learn recursion by building a decimal to binary converter/script.js
@@ -0,0 +1,49 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Шаг 1 В этом проекте вы создадите десятичную и двоичную систему счисления и познакомитесь с обеими системами счисления. Вы также узнаете о рекурсии, используя её для выполнения преобразований. Весь HTML и CSS для этого проекта предоставлены вам. Когда будете готовы начать, используйте метод .getElementById(), чтобы получить элемент ввода с идентификатором "number-input" и сохранить его в переменной numberInput. Используйте тот же метод, чтобы получить элемент кнопки с идентификатором "convert-btn" и сохранить его в переменной convertBtn, а также элемент вывода с идентификатором "result" и сохранить его в переменной result. ПРИМЕЧАНИЕ: В этом проекте будут преобразованы только положительные числа в двоичные.
+
+
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+
+
+
+
+
+
+
+
+
+ */
