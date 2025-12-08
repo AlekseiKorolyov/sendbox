7fce56a0f0d7f786d8c66702fa29713e2a630d2d
Aleksei Korolyov <a97415393@gmail.com>
2025-12-08 13:43:48 +0300

    js 12 Step 101

--- freecodecamp/JavaScript Algorithms and Data Structures/12 learn recursion by building a decimal to binary converter/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/12 learn recursion by building a decimal to binary converter/script.js
@@ -5,7 +5,10 @@ const animationContainer = document.getElementById("animation-container");
 const animationData = [
     {
         inputVal: 5,
-        addElDelay: 1000
+        addElDelay: 1000,
+        msg: "decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack.",
+        showMsgDelay: 15000,
+        removeElDelay: 20000
     },
     {
         inputVal: 2,
@@ -1303,6 +1306,24 @@ decimalToBinary(${obj.inputVal})
     msg: "decimalToBinary(2) returns '1' + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.",
     showMsgDelay: 10000,
     removeElDelay: 15000
+
+
+
+
+
+Шаг 101
+
+Для последнего объекта анимации добавьте свойство msg set к следующей строке:
+Пример кода
+
+"decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack."
+
+Также добавьте свойство showMsgDelay со значением 15000 и свойство removeElDelay со значением 20000.
+
+
+    msg: "decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack.",
+    showMsgDelay: 15000,
+    removeElDelay: 20000
 
 
 
