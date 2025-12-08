866f14403be81dd430a367184489fc8af3d40ebf
Aleksei Korolyov <a97415393@gmail.com>
2025-12-08 09:37:22 +0300

    js 12 Step 78

--- freecodecamp/JavaScript Algorithms and Data Structures/12 learn recursion by building a decimal to binary converter/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/12 learn recursion by building a decimal to binary converter/script.js
@@ -18,19 +18,19 @@ const checkUserInput = () => {
     const inputInt = parseInt(numberInput.value);
     if (
         !numberInput.value ||
-        isNaN(parseInt(numberInput.value)) ||
-        parseInt(numberInput.value) < 0
+        isNaN(inputInt) ||
+        inputInt < 0
     ) {
         alert("Please provide a decimal number greater than or equal to 0");
         return;
     }
 
-    if (parseInt(numberInput.value) === 5) {
+    if (inputInt === 5) {
         showAnimation();
         return;
     }
 
-    result.textContent = decimalToBinary(parseInt(numberInput.value));
+    result.textContent = decimalToBinary(inputInt);
     numberInput.value = "";
 };
 
@@ -1014,6 +1014,14 @@ return;
 
 
 const inputInt = parseInt(numberInput.value);
+
+
+
+
+
+Шаг 78
+
+Замените все вхождения parseInt(numberInput.value) на inputInt.
 
 
 
