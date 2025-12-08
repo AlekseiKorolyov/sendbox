b25ec22723d02bc33e013e123770f4395ec06206
Aleksei Korolyov <a97415393@gmail.com>
2025-12-08 12:42:55 +0300

    js 12 Step 88

--- freecodecamp/JavaScript Algorithms and Data Structures/12 learn recursion by building a decimal to binary converter/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/12 learn recursion by building a decimal to binary converter/script.js
@@ -26,15 +26,7 @@ const decimalToBinary = (input) => {
 };
 
 const showAnimation = () => {
-    setTimeout(() => {
-        console.log("free");
-    }, 500);
-    setTimeout(() => {
-        console.log("Code");
-    }, 1000);
-    setTimeout(() => {
-        console.log("Camp");
-    }, 1500);
+
 };
 
 const checkUserInput = () => {
@@ -1149,6 +1141,12 @@ setTimeout(() => {
 Шаг 87
 
 Теперь приступим к созданию самой анимации. Сначала используйте метод document.getElementById(), чтобы выбрать элемент с идентификатором animation-container и присвоить его переменной animationContainer.
+
+
+
+
+
+Шаг 88 Далее очистите функцию showAnimation(), удалив все вызовы setTimeout().
 
 
 
