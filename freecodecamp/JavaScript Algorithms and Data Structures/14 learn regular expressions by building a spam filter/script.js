e6c61ed145c92f635557e84e26709588d118c3df
Aleksei Korolyov <a97415393@gmail.com>
2025-12-11 09:51:35 +0300

    js 14 Step 1

---
+++ freecodecamp/JavaScript Algorithms and Data Structures/14 learn regular expressions by building a spam filter/script.js
@@ -0,0 +1,37 @@
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Шаг 1 Для начала проекта используйте метод `.getElementById()`, чтобы получить элементы `#message-input`, `#result` и `#check-message-btn` из HTML-документа и присвойте их переменным `messageInput`, `result` и `checkMessageButton` соответственно.
+
+
+
+ */
