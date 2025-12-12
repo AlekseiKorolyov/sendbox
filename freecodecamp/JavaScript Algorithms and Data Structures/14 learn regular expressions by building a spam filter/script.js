13c2b76445ce18da440d79b0342d9b82679d8635
Aleksei Korolyov <a97415393@gmail.com>
2025-12-12 08:19:15 +0300

    js 14 Step 3

--- freecodecamp/JavaScript Algorithms and Data Structures/14 learn regular expressions by building a spam filter/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/14 learn regular expressions by building a spam filter/script.js
@@ -30,7 +30,39 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 
 /*
-Шаг 1 Для начала проекта используйте метод `.getElementById()`, чтобы получить элементы `#message-input`, `#result` и `#check-message-btn` из HTML-документа и присвойте их переменным `messageInput`, `result` и `checkMessageButton` соответственно.
+Шаг 1
+
+Для начала проекта используйте метод `.getElementById()`, чтобы получить элементы `#message-input`, `#result` и `#check-message-btn` из HTML-документа и присвойте их переменным `messageInput`, `result` и `checkMessageButton` соответственно.
+
+
+
+
+
+Шаг 2
+
+Прикрепите обработчик событий к вашей кнопке checkMessageButton, отслеживающий событие клика. Добавьте к нему пустую функцию обратного вызова.
+
+
+
+
+
+Шаг 3
+
+Если поле messageInput пустое, выведите пользователю сообщение "Пожалуйста, введите сообщение." Затем завершите выполнение функции.
+
+
+checkMessageButton.addEventListener("click", () => {
+  if (messageInput.value === "") {
+    alert("Please enter a message.");
+    return;
+  }
+});
+
+
+
+
+
+
 
 
 
