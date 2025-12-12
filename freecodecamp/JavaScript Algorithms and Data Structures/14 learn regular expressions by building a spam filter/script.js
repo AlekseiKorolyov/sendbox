a46bffa120c569a3b649303c29149dd6d705cf48
Aleksei Korolyov <a97415393@gmail.com>
2025-12-12 08:35:13 +0300

    js 14 Step 5

--- freecodecamp/JavaScript Algorithms and Data Structures/14 learn regular expressions by building a spam filter/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/14 learn regular expressions by building a spam filter/script.js
@@ -9,6 +9,8 @@ checkMessageButton.addEventListener("click", () => {
         alert("Please enter a message.");
         return;
     }
+    result.textContent = isSpam(messageInput.value) ? "Oh no! This looks like a spam message." : "This message does not seem to contain any spam.";
+    messageInput.value = "";
 });
 
 
@@ -75,6 +77,29 @@ checkMessageButton.addEventListener("click", () => {
 
 
 const isSpam = (msg) => false;
+
+
+
+
+
+Шаг 5
+
+В обработчике событий необходимо обновить текст элемента результата. Для этого можно использовать тернарный оператор. Вот пример присвоения результата тернарного оператора текстовому содержимому элемента:
+Пример кода
+
+el.textContent = condition ? "Use this text if the condition is true" : "Use this text if the condition is false";
+
+После оператора if используйте тернарный оператор для проверки истинности вызова функции isSpam() с аргументом messageInput.value. Если истинно, установите свойство textContent элемента результата в значение "О нет! Это похоже на спам-сообщение." В противном случае установите его в значение "Это сообщение, похоже, не содержит спама." Затем установите свойство value элемента messageInput в пустую строку.
+
+
+checkMessageButton.addEventListener("click", () => {
+  if (messageInput.value === "") {
+    alert("Please enter a message.");
+    return;
+  }
+  result.textContent = isSpam(messageInput.value) ? "Oh no! This looks like a spam message." : "This message does not seem to contain any spam.";
+  messageInput.value = "";
+});
 
 
 
