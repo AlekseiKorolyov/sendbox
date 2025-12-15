a2e1cb02fdc25f3b5f0f21d152db19b49024f675
Aleksei Korolyov <a97415393@gmail.com>
2025-12-15 13:49:27 +0300

    js 14 Step 35

--- freecodecamp/JavaScript Algorithms and Data Structures/14 learn regular expressions by building a spam filter/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/14 learn regular expressions by building a spam filter/script.js
@@ -6,8 +6,9 @@ const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
 const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:\s|$)/i;
 const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+const dearRegex = /dear friend/i;
 
-const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
@@ -430,6 +431,19 @@ const stockRegex = /[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7]/i;
 
 
 const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+
+
+
+
+
+Шаг 35
+
+Ваше итоговое регулярное выражение будет искать строки типа «дорогой друг». Объявите класс dearRegex и присвойте ему регулярное выражение, которое будет соответствовать строке «дорогой друг». Не забудьте сделать его нечувствительным к регистру и добавить его в массив denyList.
+
+
+const dearRegex = /dear friend/i;
+
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
 
 
