4e2a53d7a4bc18811583d046eb1e737a9860fdca
Aleksei Korolyov <a97415393@gmail.com>
2025-12-15 10:35:23 +0300

    js 14 Step 17

--- freecodecamp/JavaScript Algorithms and Data Structures/14 learn regular expressions by building a spam filter/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/14 learn regular expressions by building a spam filter/script.js
@@ -3,7 +3,7 @@ const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
-const dollarRegex = /[0-9]+ dollars/i;
+const dollarRegex = /[0-9]+ hundred|thousand|million|billion dollars/i;
 
 const denyList = [helpRegex, dollarRegex];
 
@@ -229,6 +229,17 @@ const dollarRegex = /[0-9] dollars/i;
 
 
 const dollarRegex = /[0-9]+ dollars/i;
+
+
+
+
+
+Шаг 17
+
+Между цифрами и текстом, обозначающим доллары, необходимо отображать разряды. Используйте символ | для обозначения сотен, тысяч, миллионов или миллиардов между цифрами и долларами.
+
+
+const dollarRegex = /[0-9]+ hundred|thousand|million|billion dollars/i;
 
 
 
