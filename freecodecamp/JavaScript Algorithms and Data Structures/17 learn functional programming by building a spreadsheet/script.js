0df97145c392d5fcb60408c690d2fac11f1c6551
Aleksei Korolyov <a97415393@gmail.com>
2026-01-20 12:18:19 +0300

    js 17 Step 54

--- freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
@@ -26,7 +26,7 @@ const evalFormula = (x, cells) => {
     const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
     const elemValue = num => character => idToText(character + num);
     const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
-    const rangeExpanded = x.replace(rangeRegex);
+    const rangeExpanded = x.replace(rangeRegex, (match) => {});
 }
 
 window.onload =  () => {
@@ -688,6 +688,17 @@ const addCharacters = character1 => character2 => num => charRange(character1, c
 
 
 const rangeExpanded = x.replace(rangeRegex);
+
+
+
+
+Шаг 54
+
+Второй аргумент метода `.replace()` не обязательно должен быть строкой. Вместо этого вы можете передать функцию обратного вызова для выполнения более сложной логики над найденной строкой. Функция обратного вызова принимает несколько параметров. Первый — это найденная строка. Передайте пустую функцию обратного вызова в вызов `.replace()` и укажите в качестве параметра `match`.
+
+
+const rangeExpanded = x.replace(rangeRegex, (match) => {});
+
 
 
 
