6f7156889b9c929bf3a414deba136346d8784958
Aleksei Korolyov <a97415393@gmail.com>
2026-01-20 13:31:24 +0300

    js 17 Step 63

--- freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
@@ -28,7 +28,7 @@ const evalFormula = (x, cells) => {
     const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
     const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
     const cellRegex = /[A-J][1-9][0-9]?/gi;
-    const cellExpanded = rangeExpanded.replace(cellRegex, (match) => {});
+    const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
 }
 
 window.onload =  () => {
@@ -785,6 +785,16 @@ const cellRegex = /[A-J][1-9][0-9]?/gi;
 
 
 const cellExpanded = rangeExpanded.replace(cellRegex, (match) => {});
+
+
+
+
+Шаг 63
+
+Обновите свою функцию обратного вызова, чтобы она возвращала результат вызова idToText() с аргументом match. Помните, что ваше регулярное выражение нечувствительно к регистру, поэтому вам нужно будет вызвать toUpperCase() для match перед передачей его в idToText().
+
+
+const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
 
 
 
