603755be4ac4f40dfd8933a0b3a88f60546de099
Aleksei Korolyov <a97415393@gmail.com>
2026-01-20 08:08:05 +0300

    js 17 Step 42

--- freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
@@ -24,6 +24,7 @@ const evalFormula = (x, cells) => {
     const idToText = (id) => cells.find(cell => cell.id === id ).value;
     const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
     const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+    const elemValue = (num) => {};
 }
 
 window.onload =  () => {
@@ -542,6 +543,16 @@ const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
 
 
 const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+
+
+
+
+Шаг 42
+
+Объявите функцию elemValue, которая принимает параметр типа num. Функция должна быть пустой.
+
+
+const elemValue = (num) => {};
 
 
 
