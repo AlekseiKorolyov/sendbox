de02bb9a1fb343bba20b0156109c3f226b0fb15a
Aleksei Korolyov <a97415393@gmail.com>
2026-01-20 12:43:27 +0300

    js 17 Step 58

--- freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
@@ -26,7 +26,7 @@ const evalFormula = (x, cells) => {
     const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
     const elemValue = num => character => idToText(character + num);
     const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
-    const rangeExpanded = x.replace(rangeRegex, (match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters));
+    const rangeExpanded = x.replace(rangeRegex, (match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)));
 }
 
 window.onload =  () => {
@@ -728,6 +728,16 @@ const rangeExpanded = x.replace(rangeRegex, (match, char1, num1, char2, num2) =>
 
 
 rangeFromString(num1, num2).map(addCharacters)
+
+
+
+
+Шаг 58
+
+Функция addCharacters возвращает функцию, поэтому вам нужно будет её вызвать. Передайте char1 в качестве аргумента
+
+
+addCharacters(char1)
 
 
 
