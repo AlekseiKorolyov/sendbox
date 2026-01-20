ac01658eb4c91e664b811a8fa5ce98e9a2d191b9
Aleksei Korolyov <a97415393@gmail.com>
2026-01-20 08:14:35 +0300

    js 17 Step 43

--- freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
@@ -24,7 +24,12 @@ const evalFormula = (x, cells) => {
     const idToText = (id) => cells.find(cell => cell.id === id ).value;
     const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
     const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
-    const elemValue = (num) => {};
+    const elemValue = (num) => {
+        const inner = (character) => {
+
+        };
+        return inner;
+    };
 }
 
 window.onload =  () => {
@@ -553,6 +558,20 @@ const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
 
 
 const elemValue = (num) => {};
+
+
+
+
+Шаг 43
+
+В функции elemValue объявите функцию с именем inner, которая принимает параметр character. Затем верните вашу inner функцию.
+
+
+    const inner = (character) => {
+
+    };
+    return inner;
+
 
 
 
