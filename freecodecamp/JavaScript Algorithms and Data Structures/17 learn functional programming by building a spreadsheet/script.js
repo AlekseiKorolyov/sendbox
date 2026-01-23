c411ce344045a1972d087128248b705fca07767e
Aleksei Korolyov <a97415393@gmail.com>
2026-01-23 10:13:20 +0300

    js 17 Step 105

--- freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/17 learn functional programming by building a spreadsheet/script.js
@@ -1,46 +1,48 @@
 const infixToFunction = {
-    "+" : (x, y) => x + y,
+    "+": (x, y) => x + y,
     "-": (x, y) => x - y,
     "*": (x, y) => x * y,
     "/": (x, y) => x / y,
-};
+}
 
 const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
 
 const highPrecedence = str => {
-    const regex = /^\d+[*\/\d+$]/;
+    const regex = /([\d.]+)([*\/])([\d.]+)/;
     const str2 = infixEval(str, regex);
     return str === str2 ? str : highPrecedence(str2);
-};
+}
 
-const isEven = (num) => num % 2 === 0;
-const sum = (nums) => nums.reduce((acc, el) => acc + el);
-const average = (nums) => sum(nums) / nums.length;
+const isEven = num => num % 2 === 0;
+const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+const average = nums => sum(nums) / nums.length;
 
-const median = (nums) => {
+const median = nums => {
     const sorted = nums.slice().sort((a, b) => a - b);
     const length = sorted.length;
     const middle = length / 2 - 1;
     return isEven(length)
         ? average([sorted[middle], sorted[middle + 1]])
         : sorted[Math.ceil(middle)];
-};
+}
 
 const spreadsheetFunctions = {
+
     sum,
     average,
     median,
     even: nums => nums.filter(isEven),
-    firsttwo: nums => nums.slice(0,2),
+    someeven: nums => nums.some(isEven),
+    everyeven: nums => nums.every(isEven),
+    firsttwo: nums => nums.slice(0, 2),
     lasttwo: nums => nums.slice(-2),
     has2: nums => nums.includes(2),
     increment: nums => nums.map(num => num + 1),
-    someeven: nums => nums.some(num => num % 2 === 0),
-    everyeven: nums => nums.every(num => num % 2 === 0),
     random: ([x, y]) => Math.floor(Math.random() * y + x),
     range: nums => range(...nums),
-    nodupes: nums => [... new Set(nums)],
-};
+    nodupes: nums => [...new Set(nums).values()],
+    "": nums => nums,
+}
 
 const applyFunction = str => {
     const noHigh = highPrecedence(str);
@@ -50,13 +52,13 @@ const applyFunction = str => {
     const toNumberList = args => args.split(",").map(parseFloat);
     const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
     return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match);
-};
+}
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
-const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code) => String.fromCharCode(code));
+const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
 const evalFormula = (x, cells) => {
-    const idToText = (id) => cells.find(cell => cell.id === id ).value;
+    const idToText = id => cells.find(cell => cell.id === id).value;
     const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
     const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
     const elemValue = num => character => idToText(character + num);
@@ -68,36 +70,36 @@ const evalFormula = (x, cells) => {
     return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
 }
 
-window.onload =  () => {
+window.onload = () => {
     const container = document.getElementById("container");
     const createLabel = (name) => {
         const label = document.createElement("div");
         label.className = "label";
         label.textContent = name;
         container.appendChild(label);
-    };
+    }
     const letters = charRange("A", "J");
     letters.forEach(createLabel);
     range(1, 99).forEach(number => {
-        createLabel(number)
-        letters.forEach((letter) => {
+        createLabel(number);
+        letters.forEach(letter => {
             const input = document.createElement("input");
             input.type = "text";
             input.id = letter + number;
             input.ariaLabel = letter + number;
             input.onchange = update;
             container.appendChild(input);
-        });
-    });
-};
+        })
+    })
+}
 
-const update = (event) => {
+const update = event => {
     const element = event.target;
     const value = element.value.replace(/\s/g, "");
     if (!value.includes(element.id) && value.startsWith('=')) {
         element.value = evalFormula(value.slice(1), Array.from(document.getElementById("container").children));
     }
-};
+}
 
 
 
@@ -1250,6 +1252,16 @@ range: nums => range(...nums),
 
 
 nodupes: nums => [... new Set(nums)],
+
+
+
+
+Шаг 105
+
+Наконец, для обработки потенциальных крайних случаев добавьте пустое строковое свойство (вам потребуется использовать кавычки), которое представляет собой функцию, принимающую один аргумент и возвращающую этот аргумент. На этом ваш проект электронной таблицы завершен. Вы можете поэкспериментировать с добавлением поддержки еще большего количества функций.
+
+
+"": nums => nums,
 
 
 
