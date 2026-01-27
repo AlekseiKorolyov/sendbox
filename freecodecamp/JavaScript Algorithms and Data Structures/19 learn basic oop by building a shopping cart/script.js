589ded9de043b7c69f034e729205a02897968f21
Aleksei Korolyov <a97415393@gmail.com>
2026-01-27 14:30:04 +0300

    js 19 Step 2

--- freecodecamp/JavaScript Algorithms and Data Structures/19 learn basic oop by building a shopping cart/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/19 learn basic oop by building a shopping cart/script.js
@@ -1,7 +1,8 @@
 const cartContainer = document.getElementById("cart-container");
 const productsContainer = document.getElementById("products-container");
 const dessertCards = document.getElementById("dessert-card-container");
-
+const cartBtn = document.getElementById("cart-btn");
+const clearCartBtn = document.getElementById("clear-cart-btn");
 
 
 
@@ -33,4 +34,22 @@ const dessertCards = document.getElementById("dessert-card-container");
 Шаг 1
 
 Вы будете создавать приложение для корзины покупок. HTML и CSS уже предоставлены, но вам потребуется написать JavaScript, чтобы сделать страницу интерактивной. Для начала вам нужно получить некоторые элементы из DOM. Начните с использования document.getElementById() для получения элементов #cart-container, #products-container и #dessert-card-container. Сохраните их в переменных с именами cartContainer, productsContainer и dessertCards соответственно. Поскольку они не будут изменяться, не забудьте объявить их как const.
+
+
+
+
+Шаг 2 Теперь вам нужно получить две кнопки. Продолжая шаблон, получите элементы #cart-btn и #clear-cart-btn. Сохраните их в переменных с именами cartBtn и clearCartBtn соответственно.
+
+
+const cartBtn = document.getElementById("cart-btn");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+
+
+
+
+
+
+
+
+
  */
