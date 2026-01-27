5dd6fa2878c55d73f83b21d1227442defc0e02b1
Aleksei Korolyov <a97415393@gmail.com>
2026-01-27 19:01:00 +0300

    js 19 Step 13

--- freecodecamp/JavaScript Algorithms and Data Structures/19 learn basic oop by building a shopping cart/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/19 learn basic oop by building a shopping cart/script.js
@@ -90,9 +90,10 @@ products.forEach(
     ({name, id, price, category}) => {
         dessertCards.innerHTML += `
     <div class="dessert-card">
-      <h2>${name}</h2>
-      <p class="dessert-price">$${price}</p>
-      <p class="product-category">Category: ${category}</p>
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button id="${id}" class="btn add-to-cart-btn">Add to cart</button>
     </div>
     `;
     }
@@ -245,6 +246,16 @@ dessertCards.innerHTML += `
 
         <p class="dessert-price">$${price}</p>
         <p class="product-category">Category: ${category}</p>
+
+
+
+
+Шаг 13
+
+Наконец, после элементов <p> создайте элемент button. Присвойте ему id, равный значению переменной id, класс btn add-to-cart-btn и используйте текст "Add to cart".
+
+
+<button id="${id}" class="btn add-to-cart-btn">Add to cart</button>
 
 
 
