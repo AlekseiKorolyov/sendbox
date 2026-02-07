e7d79ea26c4ce26a4856440ec78738e5fbdccefe
Aleksei Korolyov <a97415393@gmail.com>
2026-02-07 16:50:40 +0300

    js 20 Step 1

---
+++ freecodecamp/JavaScript Algorithms and Data Structures/20 learn intermediate oop by building a platformer game/script.js
@@ -0,0 +1,33 @@
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Шаг 1
+
+В этом проекте вы изучите основы объектно-ориентированного программирования среднего уровня, создав платформенную игру. Весь HTML и CSS предоставлены. Начните с использования document.getElementById() для получения элементов #start-btn и #canvas. Сохраните их в константных переменных с именами startBtn и canvas соответственно.
+
+
+
+
+
+
+
+
+*/
