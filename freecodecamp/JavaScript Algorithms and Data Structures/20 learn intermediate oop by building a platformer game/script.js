fd0f7320537beee10a1cf59834a6e601ffc140c1
Aleksei Korolyov <a97415393@gmail.com>
2026-02-11 13:18:13 +0300

    js 20 Step 74

--- freecodecamp/JavaScript Algorithms and Data Structures/20 learn intermediate oop by building a platformer game/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/20 learn intermediate oop by building a platformer game/script.js
@@ -73,7 +73,17 @@ const player = new Player();
 
 const platformPositions = [
     { x: 500, y: proportionalSize(450) },
-    { x: 700, y: proportionalSize(400) }
+    { x: 700, y: proportionalSize(400) },
+    { x: 850, y: proportionalSize(350) },
+    { x: 900, y: proportionalSize(350) },
+    { x: 1050, y: proportionalSize(150) },
+    { x: 2500, y: proportionalSize(450) },
+    { x: 2900, y: proportionalSize(400) },
+    { x: 3150, y: proportionalSize(350) },
+    { x: 3900, y: proportionalSize(450) },
+    { x: 4200, y: proportionalSize(400) },
+    { x: 4400, y: proportionalSize(200) },
+    { x: 4700, y: proportionalSize(150) }
 ];
 
 const animate = () => {
@@ -981,6 +991,25 @@ const platformPositions = [
   { x: 500, y: proportionalSize(450) },
   { x: 700, y: proportionalSize(400) }
 ];
+
+
+
+
+Шаг 74
+
+Добавьте остальные позиции платформы в массив platformPositions со следующими значениями:
+Пример кода
+
+x=850 y=proportionalSize(350)
+x=900 y=proportionalSize(350)
+x=1050 y=proportionalSize(150)
+x=2500 y=proportionalSize(450)
+x=2900 y=proportionalSize(400)
+x=3150 y=proportionalSize(350)
+x=3900 y=proportionalSize(450)
+x=4200 y=proportionalSize(400)
+x=4400 y=proportionalSize(200)
+x=4700 y=proportionalSize(150)
 
 
 
