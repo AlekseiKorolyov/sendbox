ed01cff0510a3bb8431fb3a703e782967a8e7594
Aleksei Korolyov <a97415393@gmail.com>
2026-03-09 10:57:54 +0300

    js 23 Step 1

---
+++ freecodecamp/JavaScript Algorithms and Data Structures/23 Here's a preview of what you will build/script.js
@@ -0,0 +1,32 @@
+const authorContainer = document.getElementById("author-container");
+const loadMoreBtn = document.getElementById("load-more-btn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Шаг 1 Весь HTML и CSS для этого проекта предоставлены вам. Вы можете ознакомиться с двумя файлами, чтобы изучить их. Начните с получения элементов #author-container и #load-more-btn с помощью метода .getElementById(). Присвойте их переменным authorContainer и loadMoreBtn соответственно. Переменные не изменятся, поэтому используйте const для их объявления.
+
+
+
+
+
+
+
+
+
+ */
