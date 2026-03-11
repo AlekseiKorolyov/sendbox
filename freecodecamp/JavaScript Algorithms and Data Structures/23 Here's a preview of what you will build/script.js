d1af631446c1b1be8f4cc74338fe36238e366c62
Aleksei Korolyov <a97415393@gmail.com>
2026-03-11 09:18:09 +0300

    js 23 Step 3

--- freecodecamp/JavaScript Algorithms and Data Structures/23 Here's a preview of what you will build/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/23 Here's a preview of what you will build/script.js
@@ -2,6 +2,7 @@ const authorContainer = document.getElementById("author-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
 
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
+    .then((res) => console.log(res))
 
 
 
@@ -34,6 +35,22 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
 
 
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
+
+
+
+
+Шаг 3
+
+Метод fetch() возвращает Promise, который представляет собой объект-заполнитель, который будет либо выполнен, если ваш запрос будет успешным, либо отклонен, если ваш запрос будет неуспешным. Если Promise выполнен, он разрешается в объект Response, и вы можете использовать метод .then() для доступа к Response. Вот как можно связать метод .then() с методом fetch():
+Пример кода
+
+fetch("sample-url-goes-here")
+ .then((res) => res)
+
+Свяжите метод .then() с вашим вызовом fetch. Внутри метода .then() добавьте функцию обратного вызова с параметром res, затем выведите res в консоль, чтобы увидеть объект Response. Откройте консоль браузера и разверните объект Response, чтобы увидеть, что он содержит. Ещё раз, пока не завершайте код точкой с запятой.
+
+
+.then((res) => console.log(res))
 
 
 
