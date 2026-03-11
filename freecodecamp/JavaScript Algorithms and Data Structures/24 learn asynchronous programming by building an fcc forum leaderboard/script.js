43754d02fbd0d925a666a8345d2b48abd97e5273
Aleksei Korolyov <a97415393@gmail.com>
2026-03-11 13:35:31 +0300

    js 24 Step 1

---
+++ freecodecamp/JavaScript Algorithms and Data Structures/24 learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -0,0 +1,36 @@
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Шаг 1
+
+В этом проекте вы создадите таблицу лидеров форума freeCodeCamp, которая будет отображать последние темы, пользователей и ответы с форума freeCodeCamp. HTML и CSS предоставлены для вас. Можете свободно их изучить. Когда будете готовы, используйте const для объявления переменной forumLatest и присвойте ей строку "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json". Ниже создайте еще одну константную переменную с именем forumTopicUrl и присвойте ей строку "https://forum.freecodecamp.org/t/".
+
+
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+
+
+
+
+
+
+
+ */
