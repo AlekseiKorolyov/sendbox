3f236a55b7773a03d92ee7569aa7d2f977ecb7ed
Aleksei Korolyov <a97415393@gmail.com>
2026-03-12 09:34:33 +0300

    js 24 Step 4

--- freecodecamp/JavaScript Algorithms and Data Structures/24 learn asynchronous programming by building an fcc forum leaderboard/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/24 learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -2,7 +2,9 @@ const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
 const postsContainer = document.getElementById("posts-container");
+const fetchData = async () => {};
 
 
 
@@ -50,6 +52,16 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 
 const postsContainer = document.getElementById("posts-container");
+
+
+
+
+Шаг 4
+
+Для заполнения таблицы лидеров форума данными вам потребуется запросить данные из API. Это называется асинхронной операцией, что означает, что задачи выполняются независимо от основного потока программы. Вы можете использовать ключевое слово async для создания асинхронной функции, которая возвращает промис. Пример кода const example = async () => { console.log("this is an example"); }; Используйте ключевое слово async для создания асинхронной стрелочной функции с именем fetchData.
+
+
+const fetchData = async () => {};
 
 
 
