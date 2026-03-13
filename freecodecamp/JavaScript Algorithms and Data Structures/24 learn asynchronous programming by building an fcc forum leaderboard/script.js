04d2bb929cf34ffd4ce4c5dfc93349b0bb05dfef
Aleksei Korolyov <a97415393@gmail.com>
2026-03-13 13:44:36 +0300

    js 24 Step 31

--- freecodecamp/JavaScript Algorithms and Data Structures/24 learn asynchronous programming by building an fcc forum leaderboard/script.js
+++ freecodecamp/JavaScript Algorithms and Data Structures/24 learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -4,10 +4,20 @@ const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
+
 const allCategories = {
     299: { category: "Career Advice", className: "career" },
     409: { category: "Project Feedback", className: "feedback" },
-    417: { category: "freeCodeCamp Support", className: "support"}
+    417: { category: "freeCodeCamp Support", className: "support" },
+    421: { category: "JavaScript", className: "javascript" },
+    423: { category: "HTML - CSS", className: "html-css" },
+    424: { category: "Python", className: "python" },
+    432: { category: "You Can Do This!", className: "motivation" },
+    560: { category: "Back-End Development", className: "backend" },
+};
+
+const forumCategory = (id) => {
+
 };
 
 const timeAgo = (time) => {
@@ -438,6 +448,18 @@ const allCategories = {
 
 
 417: { category: "freeCodeCamp Support", className: "support"}
+
+
+
+
+Шаг 31
+
+Остальная часть объекта allCategories уже создана. На следующих нескольких шагах вы создадите функцию для получения названия категории из объекта allCategories. Начните с создания стрелочной функции с именем forumCategory, используя id в качестве имени параметра.
+
+
+const forumCategory = (id) => {
+
+};
 
 
 
