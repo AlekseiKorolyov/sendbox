35d159e67f2669c3a2e89d7301e87ad9f47c1936
Aleksei Korolyov <a97415393@gmail.com>
2026-03-17 14:50:24 +0300

    8 kyu You-Can't-Code-Under-Pressure-#1

---
+++ codewars/8 kyu/You-Can't-Code-Under-Pressure-#1.js
@@ -0,0 +1,24 @@
+/*
+Instructions
+
+Code as fast as you can! You need to double the integer and return it.
+
+ */
+
+// solution
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+}
+
+
+//sample tests
+
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertEquals(doubleInteger(2), 4);
+    });
+});
