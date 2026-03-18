0e15207e8922a0cb020b9d41c11949ecb0bcfbdd
Aleksei Korolyov <a97415393@gmail.com>
2026-03-18 17:29:25 +0300

    8 kyu Multiply

---
+++ codewars/8 kyu/multiply.js
@@ -0,0 +1,33 @@
+/*
+Instructions
+
+This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+a * b;
+}
+
+ */
+
+
+//Solution
+
+function multiply(a, b){
+    return a * b;
+}
+
+//Simple tests
+
+const assert = require("chai").assert;
+
+describe("Multiply", () => {
+    it("fixed tests", () => {
+        assert.strictEqual(multiply(1,1), 1);
+        assert.strictEqual(multiply(2,1), 2);
+        assert.strictEqual(multiply(2,2), 4);
+        assert.strictEqual(multiply(3,5), 15);
+        assert.strictEqual(multiply(5,0), 0);
+        assert.strictEqual(multiply(0,5), 0);
+        assert.strictEqual(multiply(0,0), 0);
+    });
+});
